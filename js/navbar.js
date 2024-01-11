081482010a565ed27a02feca5103c947e7fe03ce
Ali <work.gh.2021@gmail.com>
2024-01-11 21:42:23 +0330

    navbar (css-part3-js)

--- js/navbar.js
+++ js/navbar.js
@@ -6,3 +6,23 @@ navBtn.addEventListener("click", () => {
     navLinks.classList.toggle("show-links");
 })
 
+
+// ****************** smooth scroll ******************
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        navLinks.classList.remove("show-links");
+
+        const id = e.target.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+
+        let position = element.offsetTop - 62;
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+            behavior: "smooth"
+        });
+    });
+});
